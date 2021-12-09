9ccf66b7e6607bb3f8469758a49c4496d2ad4472
Nico <nicolasimplant@gmail.com>
2021-12-09 15:18:41 -0500

    using examples for testing

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ class HairColor(Enum):
     green = "green"
     blue  = "blue"
 
+
 class Location(BaseModel):
 
     city: str = Field(
@@ -50,45 +51,67 @@ class Location(BaseModel):
 class Person(BaseModel):
 
     # Parametrizamos la informacion que debe tener cada uno de los atributos de nuestra clase
+    # Utilizamos example para ingresar datos para realizar pruebas
 
     first_name : str = Field(
         ...,
         min_length=1,
         max_length=50,
+        example = 'Nicolas'
         )
 
     last_name : str = Field(
         ...,
         min_length=1,
         max_length=50,
+        example = 'Implant'
         )
 
     age: int = Field(
         ...,
         gt= 0,
         le=115,
+        example = 25
     )
 
     email: EmailStr = Field(
         ...,
         title= 'User email',
-        description= 'Email from user'
+        description= 'Email from user',
+        example = 'nicolas@implant.com'
     )
 
     # Los siguientes son valores opcionales
-    hair_color : Optional[HairColor] = Field(default=None)    
-    is_married: Optional[bool] = Field(default=None)
+    hair_color : Optional[HairColor] = Field(default=None, example = HairColor.black)    
+    is_married: Optional[bool] = Field(default=None, example = False)
 
     # Validando tipos de datos especiales
-    credict_card: Optional[PaymentCardNumber] = Field(
-        default=None,
-        title= 'Credict Card Number',
-        description= 'use if you want to link a credit card for your account')
+    # credict_card: Optional[PaymentCardNumber] = Field(
+    #     default=None,
+    #     title= 'Credict Card Number',
+    #     description= 'use if you want to link a credit card for your account')
 
     website: Optional[HttpUrl] = Field(
         default= None,
         title= 'User Website',
-        description= 'use if you want to link your website in your account')
+        description= 'use if you want to link your website in your account',
+        example = 'https://www.google.com/'
+        )
+
+    # Se declara una clase para realizar las prubeas sobre fastAPI generando un ejemplo de prueba, 
+    # sin embargo es posible realizarlo de manera distinta directamente en los atributos de la clase
+
+    # class Config:
+    #     schema_extra = {
+    #         'example': {
+    #             'first_name' : 'Nicolas',
+    #             'last_name'  : 'Implant',
+    #             'age' : 30,
+    #             'email': 'nicolas@implant.com',
+    #             'hair_color' : 'brown',
+    #             'is_married': False
+    #         }
+    #     }
 
 # Path operation decorator, este decorador utiliza el metodo .get() para modificar la funcion home, que será el lugar al cual 
 # ingresaran los usuarios de nuesta app y retorna un archivo JSON
@@ -165,16 +188,16 @@ def update_person(
         gt= 0
     ),
     person: Person = Body(...),
-    location: Location = Body(...)
+    
 ):
 
     # Generamos un único diccionario para retornar los archivos JSON con el resultado de los 2 request body "person & location"   
     
-    results = person.dict()
-    results.update(location.dict())
+    #results = person.dict()
+    #results.update(location.dict())
 
     # Si bien los diccionarios en python se pueden combinar de con una sintaxis mas sencilla como:
     # "person.dict() & location.dict()" FastAPI no soporta un este tipo de declaraciones, por lo tanto debe ser de la manera 
     # más clasica 
     
-    return results
+    return person
