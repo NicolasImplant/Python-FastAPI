fe9b9d12fa128854564859687d9082f341b683f4
Nico <nicolasimplant@gmail.com>
2021-12-08 20:02:18 -0500

    Validations query

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 # FastAPI
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query
 
 # Inicializamos la variable con una instancia de fastAPI, de esta manera se crea un objeto de la clase fastAPI y se asigna
 # la variable app
@@ -37,3 +37,16 @@ def home():
 # Request Body, debido a la notación (...) indica que el parametro o el atributo son obligatorios
 def create_person(person: Person = Body(...)): 
     return person
+
+# Validation query parameters
+
+@app.get("/person/detail")
+
+    # Funcion para mostrar persona en el endpoint, por default el nombre sera nulo o "None", y sus carateristicas son
+    # de tipo string con un mínimo de un caracter y un maximo de 50. Si bien lo ideal es que un query parameter sea opcional
+    # existen casos en los que puede ser obligatorio
+
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age : int = Query(...)):
+    return {name: age}
